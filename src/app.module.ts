0db003b50431cad7218b384bc9805ecf5184e0e9
hoach-linux <hoachprogrammer@gmail.com>
2023-03-18 00:14:41 +0700

    added posgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
+import { UsersModule } from './users/users.module';
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ".env"
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [],
+            autoLoadModels: true
+        }),
+        UsersModule
+    ],
 })
 export class AppModule {
 
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module"
 
 
 async function start() {
-    const PORT = process.env.PORT || 8080
+    const PORT = process.env.PORT || 8000
     const app = await NestFactory.create(AppModule)
 
     await app.listen(PORT, () => console.log("server running at port: " + PORT))
