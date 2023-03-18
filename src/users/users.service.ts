255828b283d788422b32e804e0096753b1826200
hoach-linux <hoachprogrammer@gmail.com>
2023-03-18 20:28:31 +0700

    added crud api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./users.model";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+
+    return user;
+  } 
+  async getUsers() {
+    const users = await this.userRepository.findAll();
+
+    return users;
+  }
+}
